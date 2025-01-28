ac5aa1926d8adce20a2c318bb0abb9a65c08e174
Kaito Nagai <gxggrazxgh@i.softbank.jp>
2025-01-28 19:49:46 +0900

    Implement HomeScreen and integrate SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart'; // providerパッケージをインポート
 import 'providers/favorites_provider.dart'; // FavoritesProviderをインポート
-import 'package:flutter/foundation.dart';
-import 'pages/splash_screen.dart'; // SplashScreenをインポート
+import 'pages/home_initial.dart'; // HomeInitialScreenをインポート
 
 void main() {
   runApp(
@@ -15,21 +14,16 @@ void main() {
   );
 }
 
-
-
 // アプリ全体のエントリーポイント
 class FishingApp extends StatelessWidget {
   const FishingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if (kDebugMode) {
-      debugPrint('FishingApp: Building...');
-    }
     return MaterialApp(
       title: 'Fishing App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SplashScreen(), // ホーム画面を指定
+      home: const HomeInitialScreen(), // 初期画面をHomeInitialScreenに変更
     );
   }
 }
