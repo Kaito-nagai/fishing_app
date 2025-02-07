7c76d6aeaef161a2407196ef6217bc846d236389
Kaito Nagai <gxggrazxgh@i.softbank.jp>
2025-02-07 19:39:12 +0900

    “少し修正

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -5,37 +5,168 @@ class BottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: Colors.black,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'ホーム',
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Stack(
+      children: [
+        // 背景用の装飾
+        Positioned(
+          left: 0,
+          top: screenHeight * 0.90,
+          child: Opacity(
+            opacity: 0.1,
+            child: Container(
+              width: screenWidth,
+              height: screenHeight * 0.3,
+              decoration: const BoxDecoration(
+                color: Color(0xFFD9D9D9),
+              ),
+            ),
+          ),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: '検索',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite),
-          label: 'マイリスト',
+        // ボトムナビゲーションのアイコン部分
+        Positioned(
+          left: screenWidth * 0.07,
+          top: screenHeight * 0.905,
+          child: SizedBox(
+            width: screenWidth * 0.85,
+            height: screenHeight * 0.08,
+            child: Stack(
+              children: [
+                // マイリストアイコン
+                Positioned(
+                  left: screenWidth * 0.67,
+                  top: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      debugPrint('マイリストアイコンがタップされました');
+                    },
+                    child: SizedBox(
+                      width: screenWidth * 0.18,
+                      height: screenHeight * 0.08,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: screenHeight * 0.037,
+                            child: Text(
+                              'マイリスト',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: const Color(0xFF777777),
+                                fontSize: screenWidth * 0.035,
+                                fontFamily: 'Noto Sans JP',
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: screenWidth * 0.055,
+                            top: screenHeight * 0.002,
+                            child: Icon(
+                              Icons.favorite,
+                              size: screenWidth * 0.07,
+                              color: const Color(0xFF777777),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                // 検索アイコン
+                Positioned(
+                  left: screenWidth * 0.35,
+                  top: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/search'); // 検索画面への遷移
+                    },
+                    child: SizedBox(
+                      width: screenWidth * 0.12,
+                      height: screenHeight * 0.07,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 1,
+                            top: screenHeight * 0.037,
+                            child: Text(
+                              '検索',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: const Color(0xFF777777),
+                                fontSize: screenWidth * 0.035,
+                                fontFamily: 'Noto Sans JP',
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: screenWidth * 0.03,
+                            top: screenHeight * 0.002,
+                            child: Icon(
+                              Icons.search,
+                              size: screenWidth * 0.07,
+                              color: const Color(0xFF777777),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                // ホームアイコン
+                Positioned(
+                  left: 8,
+                  top: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/'); // ホーム画面への遷移
+                    },
+                    child: SizedBox(
+                      width: screenWidth * 0.15,
+                      height: screenHeight * 0.08,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 5,
+                            top: screenHeight * 0.037,
+                            child: Text(
+                              'ホーム',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenWidth * 0.035,
+                                fontFamily: 'Noto Sans JP',
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: screenWidth * 0.035,
+                            top: screenHeight * 0.002,
+                            child: Icon(
+                              Icons.home,
+                              size: screenWidth * 0.07,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ],
-      onTap: (index) {
-        switch (index) {
-          case 0:
-            Navigator.pushNamed(context, '/');
-            break;
-          case 1:
-            Navigator.pushNamed(context, '/search');
-            break;
-          case 2:
-            debugPrint('マイリストがタップされました');
-            break;
-        }
-      },
     );
   }
 }
